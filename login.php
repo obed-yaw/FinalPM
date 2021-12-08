d9d774e2d0e762395383600f34c49c954f4548fd
obed-yaw <obed.agyeman@ashesi.edu.gh>
2021-12-09 03:39:45 +1200

    Changes made

--- login.php
+++ login.php
@@ -22,9 +22,9 @@
 
       //alert(xhttp.responseText);
       if(xhttp.responseText == "")
-        window.location.href = "http://localhost/Pharmacy-Management/index.html";
+        window.location.href = "http://localhost/FinalPM/index.html";
       if(xhttp.responseText == "true")
-        window.location.href = "http://localhost/Pharmacy-Management/home.php";
+        window.location.href = "http://localhost/FinalPM/home.php";
 
     </script>
   </head>
